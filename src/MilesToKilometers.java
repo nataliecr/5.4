4fd613039f0f63573566f52d4724951bc5df544e
nataliecr <pikayoshi23@gmail.com>
2016-03-07 13:38:59 -0800

    Update MilesToKilometers.java
    fixed spacing issues

--- src/MilesToKilometers.java
+++ src/MilesToKilometers.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class MilesToKilometers {
   public static void main(String[] args){
     int miles = 1;
-    System.out.println("Miles         Kilometers");
+    System.out.printf("%-10s%15s\n", "Miles", "Kilometers");
     
     while (miles <= 10){
       double kilometers = miles * 1.609;  
-      System.out.printf(miles + "             %.3f \n", kilometers);
+      System.out.printf("%-15d%-1.3f \n", miles, kilometers);
       miles++;
     } // while
     
